571e0aec18158d8381592125bd0c3e59584d2868
Peter Morlion <peter.morlion@live.be>
2015-03-07 10:27:55 +0100

    nl resources (not translated yet) + FoodItem class

---
+++ Voedselkalender/Voedselkalender.Shared/DataModel/FoodItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Voedselkalender.DataModel
+{
+    public class FoodItem
+    {
+        public string Name { get; set; }
+
+        public List<int> Months { get; set; }
+
+        public string WikipediaKey { get; set; }
+    }
+}
