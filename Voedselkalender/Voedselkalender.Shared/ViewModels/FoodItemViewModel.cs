eefd207938247525d26fec5045c0ddd7b06467a1
Peter Morlion <peter.morlion@live.be>
2015-03-10 13:12:24 +0100

    added viewmodels

---
+++ Voedselkalender/Voedselkalender.Shared/ViewModels/FoodItemViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Voedselkalender.ViewModels
+{
+    public class FoodItemViewModel : INotifyPropertyChanged
+    {
+        public FoodItemViewModel()
+        {
+        }
+
+        public FoodItemViewModel(string name)
+        {
+            // TODO: localization
+            Name = name;
+            Key = name;
+        }
+
+        public string Name { get; set; }
+
+        public string Key { get; private set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Voedselkalender/Voedselkalender.Shared/ViewModels/MonthViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using Voedselkalender.DataModel;
+
+namespace Voedselkalender.ViewModels
+{
+    public class MonthViewModel : INotifyPropertyChanged
+    {
+        private string _name;
+
+        /// <remarks>Apparently, binding to Name doesn't work.</remarks>
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        public MonthViewModel()
+        {
+        }
+
+        public MonthViewModel(string monthName, IEnumerable<FoodItem> food)
+        {
+            // TODO: localization
+            Name = monthName;
+
+            var foodList = new List<FoodItemViewModel>();
+            foreach (var foodItem in food)
+            {
+                foodList.Add(new FoodItemViewModel(foodItem.Name));
+            }
+
+            FoodItems = new ObservableCollection<FoodItemViewModel>(foodList.OrderBy(x => x.Name));
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("Name"));
+            }
+        }
+
+        public ObservableCollection<FoodItemViewModel> FoodItems { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
